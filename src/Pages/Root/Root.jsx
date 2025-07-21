6723836322d75078e4056673949d77a8f3cfae4b
julkarnine3 <julkarninemdtarek@gmail.com>
2025-07-22 05:54:18 +0600

    Added header, footer and banner

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -4,9 +4,11 @@ import { Outlet } from 'react-router';
 import Footer from '../../Components/Footer/Footer';
 const Root = () => {
     return (
-        <div>
-            <NavBar></NavBar>
-            <Outlet></Outlet>
+        <div >
+           <div className='w-11/12 mx-auto'>
+             <NavBar ></NavBar>
+             <Outlet></Outlet>
+           </div>
             <Footer></Footer>
         </div>
     );
